a5bc6321c2cdc4f61a37191d1f1292e41ef8b143
mfathony115 <mfathony115@gmail.com>
2025-08-24 12:27:08 +0700

    add workflows

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
 import Layout from "../layouts"
 import Home from "../pages/home"
+// const basename = import.meta.env.DEV ? "/" : "/personal.io"
+const basename = import.meta.env.DEV ? "/" : "/easyfolio"
+
 export const router = createBrowserRouter([
     {
         path: "/",
@@ -12,4 +15,6 @@ export const router = createBrowserRouter([
             }
         ]
     }
-])
+], {
+    basename: basename
+})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+//const basename = process.env.NODE_ENV === 'production' ? '/personal.io/' : '/';
+const basename = process.env.NODE_ENV === 'production' ? '/easyfolio/' : '/';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: basename, // Sesuai nama repository kamu
+  build: {
+    outDir: 'dist'
+  }
 })
